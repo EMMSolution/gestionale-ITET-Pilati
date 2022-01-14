058d5c5d16e3c682c25d974385ca6f0cc9be5312
s3rgi0s <marco2006bertagnolli@gmail.com>
2022-01-14 18:58:47 +0100

    2

--- server.js
+++ server.js
@@ -1,34 +1,41 @@
 const http = require("http");
-//const mysql = require("mysql");
-const gpio = require("onoff").Gpio;
-var led1 = new gpio(4, "out");  /*
-var query1;
-var impostazioniDB = {
-    host: "localhost",
-    user: "root",
+const mysql = require("mysql");
+const fs = require("fs");
+// var passwordDB1 = process.env.passDB;
+
+const impostazioniDB1 = {
+    host: "192.168.1.154",
+    user: "berta",
     password: "SERVER2006berta",
     database: "gestionalePilati"
-}
-var databaseRasp = mysql.createConnection(impostazioniDB);
+};
 
-databaseRasp.connect(function(err){
-    databaseRasp.query("SELECT * FROM elaborati;", function(err, result, fields){
-        
-    });
-});
-*/
+var database1 = mysql.createConnection(impostazioniDB1);
+
 var webServer = http.createServer(function(req, res){
-    res.writeHead(200, {"Content-Type": "text(html"});
-    res.write("tabella elaborati:");
-    res.end()
+
+    fs.readFile("home.html", function(err, data){
+        if(err) throw err;
+        res.writeHeader(200, {"Content-Type": "text/html"});
+        res.write(data)
+        res.end();
+    })
+
+    /*
+    database1.connect(function(err){
+        console.log("serverAcceso");
+        if(err) throw err;
+        console.log("database acceso");
+        database1.query("USE gestionalePilati;", function(err, result){
+        if(err) throw err;
+            console.log(result);
+            res.write(result)
+            res.end();
+        })
+    });
+    */
 });
 
 webServer.listen(8000);
-led1.writeSync(1);
 
-function spegniLed(){
-    led1.writeSync(0);
-    led1.unexport();
-}
-
-process.on("SIGINT", spegniLed);
+console.log("server web acceso");

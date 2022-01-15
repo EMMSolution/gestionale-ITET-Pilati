46a32ad24e1ee4062b0532d4ce246004a0dabb5e
s3rgi0s <marco2006bertagnolli@gmail.com>
2022-01-15 16:00:29 +0100

    ss

--- server.js
+++ server.js
@@ -1,25 +1,38 @@
 const http = require("http");
 const mysql = require("mysql");
 const fs = require("fs");
-// var passwordDB1 = process.env.passDB;
-
 const impostazioniDB1 = {
-    host: "192.168.1.154",
-    user: "berta",
-    password: "SERVER2006berta",
-    database: "gestionalePilati"
+    host     : "192.168.1.154",
+    user     : "berta",
+    password : "SERVER2006berta",
+    database : "gestionalePilati"
 };
 
-var database1 = mysql.createPool(impostazioniDB1);
+var database1 = mysql.createConnection(impostazioniDB1);
+var query1;
+var homeHTML;
+
+fs.readFile('sito/home.html', function(err, data){
+    if(err){
+        console.log(err);
+    }
+    homeHTML = data;
+});
+
+database1.connect(function(err){});
+
+database1.query("SELECT * FROM elaborati", function (err, result, fields) {
+    if(err){
+        console.log(err);
+    }
+    query1 = result;
+});
 
 var webServer = http.createServer(function(req, res){
-    database1.query("USE gestionalePilati;", function(err, result){
-        fs.readFile('/home/pi/gestionale-ITET-Pilati/sito/home.html', function(err, data) {
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write(data);
-            return res.end();
-          });
-    })
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.write(homeHTML);
+    console.log(query1);
+    res.end();
 });
 
 webServer.listen(8000);

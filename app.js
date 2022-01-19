b337cd8010ab241c88605ca1cfd2a5a386579c8c
s3rgi0s <marco2006bertagnolli@gmail.com>
2022-01-19 20:17:43 +0100

    fix dashboard login

--- app.js
+++ app.js
@@ -14,7 +14,16 @@ var app = express();
 var email = "eggsolution1@gmail.com";
 var password = "admin";
 
-var query = eval("SELECT * FROM user WHERE email='" + email + "' AND password='" + password + "'");
+var query = eval("SELECT * FROM user WHERE email='" + email + "' AND password='" + password + "';");
+
+db1.query("SELECT * FROM user;", function(err, result, fields){
+  console.log(result);
+  if(err){
+    console.log("ciaoooooooo "+err);
+  } else {
+    console.log(result + "2");
+  }
+});
 
 db1.query(query, function(err, result, fields){
   console.log(result);

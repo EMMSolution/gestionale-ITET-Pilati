ab58ea9aa9369c0d33e7d9d4e2c124895298f2b0
s3rgi0s <marco2006bertagnolli@gmail.com>
2022-01-19 14:58:58 +0100

    aggiunto login e dashboard

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,31 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {title: 'Home'});
+  	res.render('index', {title: 'Home'});
 });
 
 router.get('/login', function(req, res, next) {
-  res.render('login', {title: 'login'});
+	res.render('login', {title: 'Login'});
+});
+
+/* Login user */
+router.post('/login', function (req, res, next) {
+	const email = req.body.email;
+	const password = req.body.password;
+	
+	console.log("email: " + email);
+	console.log("pass: " + password);
+
+	if(email === "bertagnollimarco999@gmail.com" && password === "ciao") {
+		res.render('dashboard', {title: 'Dashboard'});
+	} else {
+		console.log("accesso effetuato");
+		res.render('login', {errore: 'credenziali'});
+	}
+});
+
+router.get('/dashboard', function(req, res, next) {
+	res.render('dashboard', {title: 'Login'});
 });
 
 module.exports = router;

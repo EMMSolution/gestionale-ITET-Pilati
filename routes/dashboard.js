e9788c55af9c3c742f0539bf717ff5bce23c989b
s3rgi0s <marco2006bertagnolli@gmail.com>
2022-01-19 19:37:47 +0100

    fix dashboard login

---
+++ routes/dashboard.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var db1 = require('../database/db1');
+
+// temporanea come cosa
+router.get('/elaborati', function(req, res, next){
+    res.render('dashElaborati');
+});
+
+router.post('/', function(req, res, next){
+	const email = req.body.email;
+	const password = req.body.password;
+    
+	var query = eval("SELECT * FOROM user WHERE email='" + email + "' AND password='" + password + "'");
+	console.log(query);
+
+	db1.query(query, function(err, result, fields){
+		console.log(result);
+		if(err){
+			console.log(err);
+            res.render('login', {errore: true});
+		} else {
+			res.render('dashboard', {title: "Dashboard"});
+		}
+	});
+});
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,40 +2,12 @@ var express = require('express');
 var router = express.Router();
 var db1 = require('../database/db1');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   	res.render('index', {title: 'Home'});
 });
 
 router.get('/login', function(req, res, next) {
 	res.render('login', {title: 'Login'});
-});
-
-/* Login user */
-router.post('/login', function (req, res, next) {
-	const email = req.body.email;
-	const password = req.body.password;
-	
-	console.log("email: " + email);
-	console.log("pass: " + password);
-
-	var query = eval("SELECT * FROM user WHERE email='"+email+"' AND password='"+password+"'");
-	console.log(query);
-
-	db1.query(query, function(err, result, fields){
-		if(err){
-			console.log(err);
-			res.render('login', {errore: 'Credenziali'});
-		} else {
-			res.render('dashboard', {title: 'Dashboard'});
-			console.log(result);
-		}
-	})
-
-});
-
-router.get('/dashboard', function(req, res, next) {
-	res.render('dashboard', {title: 'Login'});
 });
 
 module.exports = router;

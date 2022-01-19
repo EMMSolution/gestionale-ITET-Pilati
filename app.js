0e1e7538daf3ef9f7afe606d6efd1cfe3b2ac9a6
s3rgi0s <marco2006bertagnolli@gmail.com>
2022-01-19 20:13:19 +0100

    fix dashboard login

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var app = express();
 var email = "eggsolution1@gmail.com";
 var password = "admin";
 
-var query = eval("SELECT * FOROM user WHERE email='" + email + "' AND password='" + password + "'");
+var query = eval("SELECT * FROM user WHERE email='" + email + "' AND password='" + password + "'");
 
 db1.query(query, function(err, result, fields){
   console.log(result);

966b38e3b0d1c65804728045558400d62630e86c
s3rgi0s <marco2006bertagnolli@gmail.com>
2022-04-08 15:57:52 +0200

    aggiunto .gitgnore

--- static/script/home.js
+++ static/script/home.js
@@ -1,17 +1,30 @@
-// ELEMENT HTML
-var titoloLang = document.getElementById('titoloLang')
-var iconaLang = document.getElementById('bottoneLang')
-var titoloTema = document.getElementById('titoloTema')
-var iconaTema= document.getElementById('bottoneTema')
-var main = document.getElementById('main')
-var accedi = document.getElementById('accedi')
-var registrati = document.getElementById('registrati')
-var entraGuest = document.getElementById('entraGuest')
-var sfondo = document.getElementById('sfondo')
+window.onload = function(){
+    // ELEMENT HTML
+    var sfondo = document.querySelector('sfondo');
+    // opzioni
+    var titoloLang = document.querySelector('#titoloLang');
+    var iconaLang = document.querySelector('#bottoneLang');
+    var titoloTema = document.querySelector('#titoloTema');
+    var iconaTema= document.querySelector('#bottoneTema');
+    // form
+    var home = document.querySelector('#home');
+    var login = document.querySelector('#login');
+    // home
+    var loginInput1 = document.querySelector('#loginInput1');
+    var registerInput1 = document.querySelector('#registerInput1');
+    var guestInput1 = document.querySelector('#guestInput1');
+    // login
+    var emailInputL = document.querySelector('#emailInputL');
+    var passwordInputL = document.querySelector('#passwordInputL');
+    var submitL = document.querySelector('#submitL');
 
+    // NASCONDO SEZZIONI INIZIALI
+    login.style.opacity = "0";
 
-// OPZIONE LINGUA
+    // OPZIONE LINGUA
+    
+    
+    
+    // OPZIONE TEMA
 
-
-
-// OPZIONE TEMA
+}

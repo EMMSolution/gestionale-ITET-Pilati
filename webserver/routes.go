712e63b8d64f124a9e2acf77a48aadd1e2b8d66e
s3rgi0s <marco2006bertagnolli@gmail.com>
2022-04-10 23:55:44 +0200

    tui inizziata

--- webserver/routes.go
+++ webserver/routes.go
@@ -8,8 +8,10 @@ import (
 	"net/http"
 	"database/sql"
     _"github.com/go-sql-driver/mysql"
-	template "html/template"
+	"html/template"
 	"github.com/EggSolution/gestionale-ITET-Pilati/moduli/database"
+	// servirà per tornare alla home
+	_"github.com/EggSolution/gestionale-ITET-Pilati/moduli/main"
 )
 
 // variabile globale per al connessione al database

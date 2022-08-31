9bb9055d70661f8d9072640b7a147210533c8dd5
maxglio <massimo.maglio.06@gmail.com>
2022-08-31 20:24:54 +0200

    iniziato db locale per tema

--- static/script/menuDash.js
+++ static/script/menuDash.js
@@ -88,20 +88,10 @@ window.onload = function(){
     homeStatsWidgetColorBar1 = document.querySelector('.label1 .sinistra .barra');
     homeStatsWidgetColorBar2 = document.querySelector('.label2 .sinistra .barra');
     homeStatsWidgetColorBar3 = document.querySelector('.label3 .sinistra .barra');
-
-    // colori default sfondo (tema scuro)
-    titoloSezione.style.color = "#ffffff";
-    menuFileSottomenu.style.color = "#ffffff";
-    userNameInfo.style.color = "#ffffff";
-    indicatoreMenu.style.borderColor = "#171717";
-    indicatoreMenuBefore.style.borderRight = "5px #171717 solid";
-    indicatoreMenuBefore.style.borderBottom = "5px #171717 solid";
-    indicatoreMenuAfter.style.borderRight = "5px #171717 solid";
-    indicatoreMenuAfter.style.borderTop = "5px #171717 solid";
-    sfondoDiv1.style.background = "#545454";
-    sfondoDiv2.style.background = "#444444";
-    sfondoDiv3.style.background = "#171717";
-    sfondoDiv4.style.background = "#000000";
+    //controllo quale tema è attualmente
+    tema = localStorage.getItem("color");
+    console.log("Adesso il tema è:", tema );
+    sfondo(tema);
 
     // default sezioni
     elaboratiClasse.style.display = "none";
@@ -144,6 +134,7 @@ window.onload = function(){
     // nuovo account intro page
     if(nuovoAcc == "si"){
         welcomePage.style.display = "block";
+        tema("scuro");
     } else {
         welcomePage.style.display = "none";
     }
@@ -215,7 +206,6 @@ window.onload = function(){
             break;
     }
 }
-
 // FUNCTIONS
 function sfondo(tema){
     switch(tema){
@@ -232,6 +222,8 @@ function sfondo(tema){
             sfondoDiv2.style.background = "#E3E3E3";
             sfondoDiv3.style.background = "#D3D3D3";
             sfondoDiv4.style.background = "#8F8F8F";
+            localStorage.setItem("tema", "chiaro");
+
             break;
         case "scuro":
             titoloSezione.style.color = "#ffffff"
@@ -246,6 +238,7 @@ function sfondo(tema){
             sfondoDiv2.style.background = "#444444";
             sfondoDiv3.style.background = "#171717";
             sfondoDiv4.style.background = "#000000";
+            localStorage.setItem("tema", "scuro");
             break;
         case "eggsolution":
             titoloSezione.style.color = "#303030"
@@ -260,6 +253,7 @@ function sfondo(tema){
             sfondoDiv2.style.background = "#E2BE40";
             sfondoDiv3.style.background = "#A36B00";
             sfondoDiv4.style.background = "#4E4E4E";
+            localStorage.setItem("tema", "eggsolution");
             break;
         case "blu":
             titoloSezione.style.color = "#303030"
@@ -274,7 +268,9 @@ function sfondo(tema){
             sfondoDiv2.style.background = "#70C7EC";
             sfondoDiv3.style.background = "#4863E6";
             sfondoDiv4.style.background = "#34189C";
+            localStorage.setItem("tema", "blu");
             break;
+        
     }
 }
 
